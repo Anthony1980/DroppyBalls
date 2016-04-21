919547ecbd0149e9e405ca3b502de4716224e84f
Tran Long <tranduylong@gmail.com>
2016-04-21 17:47:28 +0700

    update android

---
+++ DroppyBalls/DroppyBalls.Android/GameCenterManager.cs
@@ -0,0 +1,35 @@
+﻿using Xamarin.Forms;
+using System.Collections.Generic;
+using DroppyBalls.Android;
+using DroppyBalls.Common;
+
+[assembly: Xamarin.Forms.Dependency (typeof (GameCenterManager))]
+namespace DroppyBalls.Android
+{
+			
+	public class GameCenterManager : Java.Lang.Object, IGameCenterManager
+	{
+
+		public void ReportScore (long score, string category){
+
+		}
+		public void SubmitAchievement (string identifier, double percentComplete, string achievementName){
+
+		}
+		public void ResetAchievement (){
+
+		}
+		public void ShowLeaderBoard(){
+
+		}
+		public void SetAuthenticateHandle(){
+
+		}
+		public void UpdateHighScore (){
+
+		}
+
+
+	}
+}
+
---
+++ DroppyBalls/DroppyBalls.Android/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using Xamarin.Forms;
+using System.Collections.Generic;
+
+using DroppyBalls.Android;
+using DroppyBalls.Common;
+[assembly: Xamarin.Forms.Dependency (typeof (GameManager))]
+
+namespace DroppyBalls.Android
+{
+				
+	public class GameManager : IGameManager
+	{
+		public GameManager(){
+		}
+
+		public long GetBestScore (){
+			return 100;
+		}
+		public void SetBestScore (long score){
+
+		}
+
+		public long GetScore (){
+			return 10;
+		}
+		public void SetScore (long score){
+
+		}
+
+		public void Rating (){
+
+		}
+
+	
+	}
+}
+
